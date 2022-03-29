2f991e654993ca04918bce68be27bb20e610db3b
Maksim <maxim4344511@gmail.com>
2022-03-29 19:47:30 +0300

    init commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,7 @@
+//import './MainComponent.scss'//
+
+
+
+
+
+//export default MainComponent//
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './styles/all.scss'
+//import MainComponent from './components/MainComponent'//
+
+//onst component = MainComponent('My component text')
+
+//document.querySelector('body').appendChild(component)
