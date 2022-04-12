597e95496a06294fa0dfd62edb812f705ef57f11
Maksim <maxim4344511@gmail.com>
2022-04-12 20:34:27 +0300

    In the process of creating checkboxes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/index.scss'
 import './pug/FormElement/btn-inner/btn_inner';
+import './pug/FormElement/checkbox/checkbox-closed/checkbox-closed';
 import './test';
 
 
---
+++ src/pug/FormElement/checkbox/checkbox-closed/checkbox-closed.js
@@ -0,0 +1,20 @@
+
+
+function mFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.checkbox-closed__title')) {
+
+    var dropdowns = document.getElementsByClassName("checkbox-closed__expandable");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
