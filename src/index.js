5b79086cc699c3c8f27a722929b9c75be24d55b7
Maksim <maxim4344511@gmail.com>
2022-05-13 21:24:49 +0300

    I made the first page of the website. Made Header and Footer responsive

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './pug/FormElement/checkbox/checkbox-closed/checkbox-closed';
 import './pug/FormElement/range-slider/range-slider';
 import './pug/FormElement/pagination/pagination';
 import './test';
-
+import './pug/Cards/datepicker';
 
 
 
 import $ from "jquery";
 import './js_widgets/jquery-ui-1.13.1.custom/jquery-ui.js';
+
 import 'bootstrap';
+
 
